fc243aa886a16add3bb5b1fc708ce317e0510e6b
Yang.Jamie 楊捷閔 TAO <Yang.Jamie@inventec.com>
2023-07-28 10:31:29 +0800

    Realize reading the FRU data of the PSU as a subroutine
    Symptom/Reason:
    	-Realize reading the FRU data of the PSU as a subroutine
    	-config FRU address of PSU and PIC
    
    Root Cause:
            N/A
    
    Solution/Change:
            N/A
    
    Entry Test:
            N/A
    
    Refer Purnell_0010
    
    https://ebg-devops.inventec.com/tfs/TAO_BU5_FW5/FW5E/_git/Purnell-DOCS/commits?itemPath=%2FCommit_memo%2FPurnell_0010_Sample_code_of_PIC_by_FAE&itemVersion=GBdocs

--- firmware/Purnell.X/i2c_address.h
+++ firmware/Purnell.X/i2c_address.h
@@ -24,6 +24,11 @@
 #define FRU_SIZE_MASK               0xFF
 #define PSU1_FRU_ADDR               0x52 //Purnell_PDB_PIC_FW_Spec_V0.02.docx page 8
 #define PSU1_FRU_Slave_ADDR         0x51 //Purnell_PDB_PIC_FW_Spec_V0.02.docx page 8
+#define PIC_FRU_Slave_ADDR          0x55 //Purnell_PDB_PIC_FW_Spec_V0.02.docx page 9
+#define PSU0_FRU_ADDR               0x50 //Purnell_PDB_PIC_FW_Spec_V0.02.docx page 9
+#define PSU0_FRU_Slave_ADDR         0x50 //Purnell_PDB_PIC_FW_Spec_V0.02.docx page 9
+
+#define PIC_FRU_SIZE_BYTES          128 //at least 1 page(1 page 64byte 2page 128byte)
 
 #define I2C_SLAVE_ADDR_MASK         0x0d
 
@@ -60,9 +65,18 @@ extern FRU_DATA   fruData;
 extern uint8_t PSU_FRU_Data[FRU_SIZE_BYTES];
 
 bool SERCOM1_I2C_Callback ( SERCOM_I2C_SLAVE_TRANSFER_EVENT event, uintptr_t contextHandle );
+bool SERCOM1_FRU_Callback ( SERCOM_I2C_SLAVE_TRANSFER_EVENT event, uintptr_t contextHandle );
+
 bool SERCOM2_I2C_Callback ( SERCOM_I2C_SLAVE_TRANSFER_EVENT event, uintptr_t contextHandle );
+bool SERCOM2_FRU_Callback ( SERCOM_I2C_SLAVE_TRANSFER_EVENT event, uintptr_t contextHandle );
+
 bool SERCOM3_I2C_Callback ( SERCOM_I2C_SLAVE_TRANSFER_EVENT event, uintptr_t contextHandle );
+bool SERCOM3_FRU_Callback ( SERCOM_I2C_SLAVE_TRANSFER_EVENT event, uintptr_t contextHandle );
+
 bool SERCOM4_I2C_Callback ( SERCOM_I2C_SLAVE_TRANSFER_EVENT event, uintptr_t contextHandle );
+bool SERCOM4_FRU_Callback ( SERCOM_I2C_SLAVE_TRANSFER_EVENT event, uintptr_t contextHandle );
+
+
 
 #endif /* _I2C_ADDRESS_H */
 
